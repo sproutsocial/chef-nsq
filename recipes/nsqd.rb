5b24a8efa80bf04cb319c2a76ff34ddaf21d6b66
Sundeep Malladi <sundeep.malladi@gmail.com>
2019-01-16 13:52:51 -0600

    [WEBINF-1248] - Provide Ubuntu 18 support for nsqd role

--- recipes/nsqd.rb
+++ recipes/nsqd.rb
@@ -10,7 +10,10 @@
 
 include_recipe 'nsq'
 
-chef_gem 'semantic'
+chef_gem 'semantic' do
+  compile_time true
+end
+
 require 'semantic'
 
 nsq_release = "nsq-#{node['nsq']['version']}-#{node['nsq']['go_version']}"
@@ -25,24 +28,89 @@ directory node['nsq']['nsqd']['data_path'] do
 end
 
 if node['nsq']['setup_services']
-  template '/etc/init/nsqd.conf' do
-    action :create
-    source 'upstart.nsqd.conf.erb'
-    mode '0644'
-    # need to stop/start in order to reload config
-    if node['nsq']['reload_services']
-      notifies :stop, 'service[nsqd]', :immediately
-      notifies :start, 'service[nsqd]', :immediately
+
+  if node.platform?('ubuntu') && node['platform_version'].to_f >= 18.04
+
+    standard_error = node['nsq']['nsqd']['standard_error'].split('file:/')[1]
+    standard_output = node['nsq']['nsqd']['standard_output'].split('file:/')[1]
+
+    directory '/var/log/nsqd' do
+      action :create
+      mode '0770'
+      owner 'nsqd'
+      group 'nsqd'
+      recursive true
+    end
+
+    file standard_output do
+      action :create
+      mode '0770'
+      owner 'nsqd'
+      group 'nsqd'
+    end
+
+     file standard_error do
+      action :create
+      mode '0770'
+      owner 'nsqd'
+      group 'nsqd'
+    end
+
+
+    template '/srv/nsqd-start.sh' do
+      action :create
+      source 'nsqd-start.sh.erb'
+      mode '0550'
+      # mode '0777'
+      owner 'nsqd'
+      group 'nsqd'
+    end
+
+    template '/etc/systemd/system/nsqd.service' do
+      action :create
+      source 'systemd.nsqd.conf.erb'
+      mode '0644'
+      notifies :run, 'execute[systemctl-daemon-reload]'
+      # need to stop/start in order to reload config
+      if node['nsq']['reload_services']
+        notifies :stop, 'service[nsqd]', :immediately
+        notifies :start, 'service[nsqd]', :immediately
+      end
+    end
+
+    execute 'systemctl-daemon-reload' do
+      command 'systemctl daemon-reload'
+      action :nothing
+    end
+  else
+    template '/etc/init/nsqd.conf' do
+      action :create
+      source 'upstart.nsqd.conf.erb'
+      mode '0644'
+      # need to stop/start in order to reload config
+      if node['nsq']['reload_services']
+        notifies :stop, 'service[nsqd]', :immediately
+        notifies :start, 'service[nsqd]', :immediately
+      end
     end
   end
 
   service 'nsqd' do
-    provider Chef::Provider::Service::Upstart
-    action [:enable, :start]
-    supports stop: true, start: true, restart: true, status: true
-    # Conditionally subscribe to version updates
-    if node['nsq']['reload_services']
-      subscribes :restart, "ark[#{nsq_release}]", :delayed
+    if node.platform?('ubuntu') && node['platform_version'].to_f >= 18.04
+      provider Chef::Provider::Service::Systemd
+      action [:enable, :start]
+      supports stop: true, start: true, restart: true, status: true
+      if node['nsq']['reload_services']
+        subscribes :restart, "ark[#{nsq_release}]", :delayed
+      end
+    else
+      provider Chef::Provider::Service::Upstart
+      action [:enable, :start]
+      supports stop: true, start: true, restart: true, status: true
+      # Conditionally subscribe to version updates
+      if node['nsq']['reload_services']
+        subscribes :restart, "ark[#{nsq_release}]", :delayed
+      end
     end
   end
 end
